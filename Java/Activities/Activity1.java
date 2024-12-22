75df9e3da56147b1d319a2e9234eb9057c06d807
chandrasekharm1999 <chandrasekharm1999@gmail.com>
2024-12-22 20:01:29 +0530

    Update and rename HelloWorld.java to Activity1.java

--- Java/Activities/Activity1.java
+++ Java/Activities/Activity1.java
@@ -1,5 +1,5 @@
 
-public class HelloWorld {
+public class Activity1 {
 	public static void main(String[] args) {
 		Car car1 = new Car("black","automatic", 2024, 4, 0);
 		Car car2 = new Car("red","manual", 2024, 4,0);
